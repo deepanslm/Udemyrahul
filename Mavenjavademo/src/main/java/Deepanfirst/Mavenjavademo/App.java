923b795da523139eb241cfb3f1d6275831f6acae
deepanslm <44049710+deepanslm@users.noreply.github.com>
2020-08-21 00:59:32 +0530

    Add files via upload

---
+++ Mavenjavademo/src/main/java/Deepanfirst/Mavenjavademo/App.java
@@ -0,0 +1,13 @@
+package Deepanfirst.Mavenjavademo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Mavenjavademo/src/test/java/Deepanfirst/Mavenjavademo/deepanTest.java
@@ -0,0 +1,15 @@
+package Deepanfirst.Mavenjavademo;
+
+import org.junit.Test;
+
+public class deepanTest {
+
+	@Test
+	public static void main(String[] args) {
+		
+		System.out.println("Deepan");
+      
+		
+	}
+
+}
